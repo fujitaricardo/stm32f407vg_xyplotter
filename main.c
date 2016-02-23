db2a4e5a3e378f3ddd8074b6068484c717c58a99
Mad Buffalo <ricardofujita1@gmail.com>
2016-02-23 00:28:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "tm_stm32f4_hd44780.h"
 #include "xyplotter.h"
+#include "semihosting.h"
 
 int main(void){
-    SystemInit();
+	SystemInit();
     TM_HD44780_Init(16, 2);
     xyPlotterInit();
     calibraZero();
+
+    moveParaXY(100, 120, 'm');
     manualControl();
     return 0;
 }
