1bfe2afffa90a575f4d8e81657712f277c341777
Mad Buffalo <ricardofujita1@gmail.com>
2016-02-23 00:26:40 -0300

    Update xyplotter.h

--- xyplotter.h
+++ xyplotter.h
@@ -6,26 +6,40 @@
 #include "tm_stm32f4_hd44780.h"
 #include "defines.h"
 
+
+/*funcoes de inicializacao dos sensores e motores*/
 void motorInit(void);
 void endInit(void);
 void joystickInit(void);
 void buttonsInit(void);
 void xyPlotterInit(void);
+
+/*funcoes de leitura dos sensores*/
 uint16_t readADC(uint8_t channel);
 uint16_t readJoystick(char eixo);
 int posicaoJoystick(char eixo);
 int readJSButton(void);
-void manualControl(void);
 int readEnd(char eixo);
 int readButtons(char button);
+
+/*funcoes de funcionamento dos motores*/
 void counterclockwise(char eixo);
 void clockwise(char eixo);
 void runClockwise(int passos, char eixo);
 void runCounterclockwise(int passos, char eixo);
-int getDistancia(void);
-char getUnidade(void);
-int converteMilimetro(void);
-int convertePolegada(void);
-void setPassos(char direction);
-void runMotor(int passos, char direction);
+void runDistancia_unidade(float distancia, char unidade, char eixo, char sentido);
+void moveParaXY(float coord_x, float coord_y, char unidade);
+
+/*funcoes de funcionamento da mesa xy*/
 void calibraZero(void);
+void manualControl(void);
+void atualizaDisplayDistancia(void);
+
+/*funcoes de conversoes entre passos e unidades de medida*/
+int convertePassos_Unidade(float distancia, char unidade);
+float converteUnidade_Passos(int passos, char unidade);
+
+/*funcoes para conversao de float para string*/
+char* ftoa(char* dest, double num, int afterPoint, int sign);
+char* inc(char* s, char* e);
+void reverse(char* p, char* q);
