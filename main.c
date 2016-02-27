a5f49675d9e053185d9483e2355313d8a5c99826
Mad Buffalo <ricardofujita1@gmail.com>
2016-02-26 21:55:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,36 @@
+/**
+ *	Engenharia Eletrica - Universidade Estadual de Londrina
+ *	Mesa xy automatizada microcontrolada com stm32f407vg
+ *
+ *	autor 		Ricardo Fujita; Guilherme Brandao da Silva; Giovani Augusto de Lima Freitas; Guilherme Almeida Pessoa
+ *	version		v1.0
+ *	ide			CooCox 1.7.7
+ *
+ */
+
 #include "tm_stm32f4_hd44780.h"
 #include "xyplotter.h"
-#include "semihosting.h"
 
 int main(void){
 	SystemInit();
     TM_HD44780_Init(16, 2);
     xyPlotterInit();
     calibraZero();
+    //calibraCaneta();
+    //menu();
 
-    moveParaXY(100, 120, 'm');
-    manualControl();
+    while(1){
+        manualControl();
+    	//setDesenhaQuadrado();
+    	/*moveParaXY(15, 0, 'm');
+    	setFura();
+    	moveParaXY(15, 15, 'm');
+    	setFura();
+    	moveParaXY(0, 15, 'm');
+    	setFura();
+    	moveParaXY(0, 0, 'm');
+    	setFura();*/
+    	Delayms(250);
+    }
     return 0;
 }
