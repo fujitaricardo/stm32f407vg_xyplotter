4690f2fb6d575807e3958c36d905f169d7e03eaf
Mad Buffalo <ricardofujita1@gmail.com>
2016-02-28 21:53:37 -0300

    Update defines.h

--- defines.h
+++ defines.h
@@ -14,12 +14,12 @@
 /*Defines globais*/
 
 //delay para acionamento dos motores
-#define MOTOR_SPEED 3	//tempo em ms do motor da mesa
-#define CANETA_SPEED 1 	//tempo em ms do motor da caneta/drill
+#define MOTOR_SPEED   3 //tempo em ms do motor da mesa
+#define CANETA_SPEED  1 //tempo em ms do motor da caneta/drill
 
 //constante de conversao de passos
-#define PASSOS_mm	6	//6 passos = 1 mm
-#define PASSOS_pol	150	//150 passos = 1 pol
+#define PASSOS_mm   6   //6 passos = 1 mm
+#define PASSOS_pol  150 //150 passos = 1 pol
 
 //pinagem motores
 #define MOTOR_PORT    GPIOE
@@ -44,16 +44,16 @@
 #define END2      GPIO_Pin_12
 
 //pinagem joystick
-#define JOYSTICK_PORT	GPIOC
-#define	JSBUTTON_PORT	GPIOC
-#define JOYSTICK_X 		GPIO_Pin_4 	//analogico
-#define JOYSTICK_Y		GPIO_Pin_5 	//analogico
-#define JSBUTTON 		GPIO_Pin_1 	//digital
+#define JOYSTICK_PORT GPIOC
+#define	JSBUTTON_PORT GPIOC
+#define JOYSTICK_X    GPIO_Pin_4  //analogico
+#define JOYSTICK_Y    GPIO_Pin_5  //analogico
+#define JSBUTTON      GPIO_Pin_1  //digital
 
 //pinagem pushbuttons
-#define	BUTTON_PORT		GPIOC
-#define	BUTTON1 		GPIO_Pin_2
-#define BUTTON2 		GPIO_Pin_3
+#define	BUTTON_PORT GPIOC
+#define	BUTTON1     GPIO_Pin_2
+#define BUTTON2     GPIO_Pin_3
 
 #endif
 
@@ -61,32 +61,32 @@
 /* 4 bit mode */
 /* RS - Register select pin */
 #ifndef HD44780_RS_PIN
-#define HD44780_RS_PORT	GPIOE
-#define HD44780_RS_PIN	GPIO_PIN_0
+#define HD44780_RS_PORT GPIOE
+#define HD44780_RS_PIN  GPIO_PIN_0
 #endif
 /* E - Enable pin */
 #ifndef HD44780_E_PIN
-#define HD44780_E_PORT	GPIOE
-#define HD44780_E_PIN	GPIO_PIN_1
+#define HD44780_E_PORT  GPIOE
+#define HD44780_E_PIN   GPIO_PIN_1
 #endif
 /* Data pins */
 /* D4 - Data 4 pin */
 #ifndef HD44780_D4_PIN
-#define HD44780_D4_PORT	GPIOE
-#define HD44780_D4_PIN	GPIO_PIN_2
+#define HD44780_D4_PORT GPIOE
+#define HD44780_D4_PIN  GPIO_PIN_2
 #endif
 /* D5 - Data 5 pin */
 #ifndef HD44780_D5_PIN
-#define HD44780_D5_PORT	GPIOE
-#define HD44780_D5_PIN	GPIO_PIN_3
+#define HD44780_D5_PORT GPIOE
+#define HD44780_D5_PIN  GPIO_PIN_3
 #endif
 /* D6 - Data 6 pin */
 #ifndef HD44780_D6_PIN
-#define HD44780_D6_PORT	GPIOE
-#define HD44780_D6_PIN	GPIO_PIN_4
+#define HD44780_D6_PORT GPIOE
+#define HD44780_D6_PIN  GPIO_PIN_4
 #endif
 /* D7 - Data 7 pin */
 #ifndef HD44780_D7_PIN
-#define HD44780_D7_PORT	GPIOE
-#define HD44780_D7_PIN	GPIO_PIN_5
+#define HD44780_D7_PORT GPIOE
+#define HD44780_D7_PIN  GPIO_PIN_5
 #endif
