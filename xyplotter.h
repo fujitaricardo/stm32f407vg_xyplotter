427f7a9fe77413799d735c7fda2afa8dc66884ff
Mad Buffalo <ricardofujita1@gmail.com>
2016-02-28 21:55:53 -0300

    Update xyplotter.h

--- xyplotter.h
+++ xyplotter.h
@@ -16,38 +16,40 @@
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_hd44780.h"
 #include "defines.h"
+#include "semihosting.h"
+#include "stm32_ub_string.h"
 
-//MOTOR_A_PIN1	PE7
-//MOTOR_A_PIN2	PE9
-//MOTOR_A_PIN3	PE11
-//MOTOR_A_PIN4	PE13
+//MOTOR_A_PIN1  PE7
+//MOTOR_A_PIN2  PE9
+//MOTOR_A_PIN3  PE11
+//MOTOR_A_PIN4  PE13
 
-//MOTOR_B_PIN1	PE8
-//MOTOR_B_PIN2	PE10
-//MOTOR_B_PIN3	PE12
-//MOTOR_B_PIN4	PE14
+//MOTOR_B_PIN1  PE8
+//MOTOR_B_PIN2  PE10
+//MOTOR_B_PIN3  PE12
+//MOTOR_B_PIN4  PE14
 
-//CANETA_PIN1	PA1
-//CANETA_PIN2	PA3
-//CANETA_PIN3	PA5
-//CANETA_PIN4	PA7
+//CANETA_PIN1 PA1
+//CANETA_PIN2 PA3
+//CANETA_PIN3 PA5
+//CANETA_PIN4 PA7
 
-//END1		 	PB11
-//END2			PB12
+//END1  PB11
+//END2  PB12
 
-//JOYSTICK_X 	PC4
-//JOYSTICK_Y	PC5
-//JSBUTTON 		PC1
+//JOYSTICK_X  PC4
+//JOYSTICK_Y  PC5
+//JSBUTTON    PC1
 
-//BUTTON1		PC2
-//BUTTON2		PC3
+//BUTTON1 PC2
+//BUTTON2 PC3
 
-//HD44780_RS_PIN	PE0
-//HD44780_E_PIN		PE1
-//HD44780_D4_PIN	PE2
-//HD44780_D5_PIN	PE3
-//HD44780_D6_PIN	PE4
-//HD44780_D7_PIN	PE5
+//HD44780_RS_PIN  PE0
+//HD44780_E_PIN   PE1
+//HD44780_D4_PIN  PE2
+//HD44780_D5_PIN  PE3
+//HD44780_D6_PIN  PE4
+//HD44780_D7_PIN  PE5
 
 //1 passo = (0,17/8)mm
 //6 passos = 1 mm
@@ -89,13 +91,15 @@ void setSobeCaneta(void);
 void setDesenhaQuadrado(void);
 void manualControl(void);
 void menu(void);
+void modoPerfuracao(void);
 void atualizaDisplayDistancia(void);
 
 /*funcoes de conversoes entre passos e unidades de medida*/
 int convertePassos_Unidade(float distancia, char unidade);
 float converteUnidade_Passos(int passos, char unidade);
 
-/*funcoes para conversao de float para string*/
+/*funcoes para conversao de float para string e semihosting*/
 char* ftoa(char* dest, double num, int afterPoint, int sign);
 char* inc(char* s, char* e);
 void reverse(char* p, char* q);
+void readStringSH(char *str);
