bd379d499e6389f899fffd9898be13dff1cba426
Mad Buffalo <ricardofujita1@gmail.com>
2016-02-28 21:45:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,29 +8,13 @@
  *
  */
 
-#include "tm_stm32f4_hd44780.h"
 #include "xyplotter.h"
 
 int main(void){
-	SystemInit();
-    TM_HD44780_Init(16, 2);
     xyPlotterInit();
     calibraZero();
-    //calibraCaneta();
-    //menu();
-
-    while(1){
-        manualControl();
-    	//setDesenhaQuadrado();
-    	/*moveParaXY(15, 0, 'm');
-    	setFura();
-    	moveParaXY(15, 15, 'm');
-    	setFura();
-    	moveParaXY(0, 15, 'm');
-    	setFura();
-    	moveParaXY(0, 0, 'm');
-    	setFura();*/
-    	Delayms(250);
-    }
+    calibraCaneta();
+    menu();
+    
     return 0;
 }
